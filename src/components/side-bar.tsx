7b122c8eb3b821a19067a87c023d02861da56b0b
root <root@noah-snbr.fritz.box>
2025-02-10 01:58:53 +0100

    fixed sidebar anim

--- src/components/side-bar.tsx
+++ src/components/side-bar.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { CSSProperties } from "react";
-import { motion, AnimatePresence } from 'framer-motion';
+import { motion, AnimatePresence } from "framer-motion";
 import useSideBar from "@/hooks/use-side-bar";
 import Image from "next/image";
 import { icon } from "@/assets/assets";
@@ -10,28 +10,29 @@ import SideBarLinkEntry from "./side-bar-link-entry";
 import { usePathname } from "next/navigation";
 
 export default function Sidebar() {
-
    const { active, close } = useSideBar();
 
    const stylesheet: CSSProperties = {
       background: "rgba(0, 0, 0, .5)",
       position: "absolute",
       backdropFilter: `blur(${8}px)`,
-   }
+   };
 
    const pathname = usePathname();
 
    return (
-      <AnimatePresence
-         mode="wait">
+      <AnimatePresence mode="wait">
          {active && (
             <motion.div
+               key={pathname} // Key ensures rerender on route change
                onClick={close}
-               transition={{ type: "tween", duration: .25 }}
+               transition={{ type: "tween", duration: 0.25 }}
                initial={{ x: "-100%", opacity: 0 }}
                animate={{ x: 0, opacity: 1 }}
+               exit={{ x: "-100%", opacity: 0 }} // Add exit animation
                className="size-full flex z-50"
-               style={stylesheet}>
+               style={stylesheet}
+            >
                <div className="w-2/3 bg-darken flex flex-col">
                   <div className="flex p-4 gap-4 items-center">
                      <Image
@@ -44,7 +45,6 @@ export default function Sidebar() {
                      <p className="font-bold text-lg opacity-50"> Sidebar </p>
                   </div>
                   <div className="flex-1 flex flex-col p-4">
-
                      {/* list all links */}
                      {sideBarLinks.map((link) => (
                         <SideBarLinkEntry
@@ -53,7 +53,8 @@ export default function Sidebar() {
                            icon={link.icon}
                            href={link.href}
                            id={link.id}
-                           title={link.title} />
+                           title={link.title}
+                        />
                      ))}
                   </div>
                </div>
