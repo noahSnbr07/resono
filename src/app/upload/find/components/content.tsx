d1472a5483ad85fc79f08f746a929ccdd11138bc
root <root@noah-snbr.fritz.box>
2025-02-10 03:04:09 +0100

    more anims 2

--- src/app/upload/find/components/content.tsx
+++ src/app/upload/find/components/content.tsx
@@ -11,9 +11,9 @@ export default function Content() {
     return (
         <div className="overflow-y-scroll h-dvh">
             <p className="text-xl font-bold"> Songs - {songs.length} </p>
-            {songs.map((song: APISongResponse, i: number) => <Song song={song} index={i} />)}
+            {songs.map((song: APISongResponse, i: number) => <Song key={i} song={song} index={i} />)}
             <p className="text-xl font-bold"> Artists - {artists.length} </p>
-            {artists.map((artist: APIArtistResponse, i: number) => <Artist artist={artist} index={i} />)}
+            {artists.map((artist: APIArtistResponse, i: number) => <Artist key={i} artist={artist} index={i} />)}
         </div>
     );
 }
