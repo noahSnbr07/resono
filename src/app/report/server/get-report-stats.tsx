654c3598d93f0f269c8b17621707c78c31c549b8
root <root@noah-snbr.fritz.box>
2025-02-11 23:33:45 +0100

    song upload 1.8/2

--- src/app/report/server/get-report-stats.tsx
+++ src/app/report/server/get-report-stats.tsx
@@ -2,13 +2,13 @@
 
 import database from "@/database/database";
 import ReportStats from "../types/report-stats";
-import { report } from "@prisma/client";
+import { Report } from "@prisma/client";
 
 export default async function getReportStats(): Promise<ReportStats> {
     const reports = await database.report.findMany();
 
-    const covered = reports.filter((element: report) => element.resolved).length;
-    const important = reports.filter((element: report) => element.important).length;
+    const covered = reports.filter((element: Report) => element.resolved).length;
+    const important = reports.filter((element: Report) => element.important).length;
     const total = reports.length
 
     return { covered, total, important };
