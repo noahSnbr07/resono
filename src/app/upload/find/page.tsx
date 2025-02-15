e8f08eb0e51b11984e445b400ee87f740f623f10
root <root@noah-snbr.fritz.box>
2025-02-15 02:09:49 +0100

    ssr fix 7

--- src/app/upload/find/page.tsx
+++ src/app/upload/find/page.tsx
@@ -5,10 +5,10 @@ import Content from "./components/content";
 import getContent from "../server/get-content";
 import { SearchParams } from "next/dist/server/request/search-params";
 
+
 export default async function page(props: { searchParams: SearchParams }) {
     const searchParams = await props.searchParams;
-    const query = searchParams.id;
-
+    const query = searchParams.query;
     const { songs, artists } = await getContent({ query: String(query) });
 
     return (
